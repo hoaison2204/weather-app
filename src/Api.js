b7ff08cb78ce22b59437ddb876f3cb5611587039
Son Nguyen <hoaison2204@gmail.com>
2023-02-08 10:08:04 +0700

    edit

--- src/Api.js
+++ src/Api.js
@@ -6,39 +6,6 @@ export const geoApiOptions = {
     }
 };
 
-export const UserData = [
-    {
-        id: 1,
-        year: 2016,
-        userGain: 80000,
-        userLost: 823,
-    },
-    {
-        id: 2,
-        year: 2017,
-        userGain: 45677,
-        userLost: 345,
-    },
-    {
-        id: 3,
-        year: 2018,
-        userGain: 78888,
-        userLost: 555,
-    },
-    {
-        id: 4,
-        year: 2019,
-        userGain: 90000,
-        userLost: 4555,
-    },
-    {
-        id: 5,
-        year: 2020,
-        userGain: 4300,
-        userLost: 234,
-    },
-];
-
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
 export const WEATHER_API_KEY = '13f46a9ed2b04207dd09f07afe0a322d';
